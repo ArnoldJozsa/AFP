b25a0b19c6b42ea7c5dd5f94ebcf276e3488fdff
ArnoldJozsa <150267859+ArnoldJozsa@users.noreply.github.com>
2024-01-04 11:21:09 +0100

    Változók deklarálás

--- blackjack.js
+++ blackjack.js
@@ -3,6 +3,15 @@
 					2,3,4,5,6,7,8,9,10,10,10,10,
 					2,3,4,5,6,7,8,9,10,10,10,10,
 					2,3,4,5,6,7,8,9,10,10,10,10];
+		var jatekosKartyai;
+		var VDKartyai;
+		var Doubling = false;
+		var rendOsszeg = 1000;
+		var nyertOsszeg = 0;
+		var VDHuzhat = true;
+		var IsThereAnyBet = true;
+		var minimumBet = rendOsszeg * 0.10;
+		var activeBet = 0;
 	
 //Arnold
 //Lilli
